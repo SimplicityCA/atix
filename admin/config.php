61962bb311623bdc1a1e22bab2e92072648b8212
Joshua Fabara <joshuafabara@gmail.com>
2016-06-13 20:40:20 -0500

    - Opencart core

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/~joshuafabara/shop/admin/');
+define('HTTP_CATALOG', 'http://localhost/~joshuafabara/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/~joshuafabara/shop/admin/');
+define('HTTPS_CATALOG', 'http://localhost/~joshuafabara/shop/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/joshuafabara/Sites/shop/admin/');
+define('DIR_SYSTEM', '/Users/joshuafabara/Sites/shop/system/');
+define('DIR_IMAGE', '/Users/joshuafabara/Sites/shop/image/');
+define('DIR_LANGUAGE', '/Users/joshuafabara/Sites/shop/admin/language/');
+define('DIR_TEMPLATE', '/Users/joshuafabara/Sites/shop/admin/view/template/');
+define('DIR_CONFIG', '/Users/joshuafabara/Sites/shop/system/config/');
+define('DIR_CACHE', '/Users/joshuafabara/Sites/shop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/joshuafabara/Sites/shop/system/storage/download/');
+define('DIR_LOGS', '/Users/joshuafabara/Sites/shop/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/joshuafabara/Sites/shop/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/joshuafabara/Sites/shop/system/storage/upload/');
+define('DIR_CATALOG', '/Users/joshuafabara/Sites/shop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'atix');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
