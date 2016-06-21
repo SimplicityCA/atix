3a440746a81aaef28282d46299b6cc07cdf44574
Franklin Alexis Paula Aguirre <franklin.paula@share.com.ec>
2016-06-20 20:06:10 -0500

    rename install and init db on web server
    db is now working on simplicity server. Init commerce  with opencart

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/~franklin/atix/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/~franklin/atix/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/~franklin/atix/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/~franklin/atix/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/franklin/Sites/atix/upload/admin/');
+define('DIR_SYSTEM', '/Users/franklin/Sites/atix/upload/system/');
+define('DIR_IMAGE', '/Users/franklin/Sites/atix/upload/image/');
+define('DIR_LANGUAGE', '/Users/franklin/Sites/atix/upload/admin/language/');
+define('DIR_TEMPLATE', '/Users/franklin/Sites/atix/upload/admin/view/template/');
+define('DIR_CONFIG', '/Users/franklin/Sites/atix/upload/system/config/');
+define('DIR_CACHE', '/Users/franklin/Sites/atix/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/franklin/Sites/atix/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/franklin/Sites/atix/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/franklin/Sites/atix/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/franklin/Sites/atix/upload/system/storage/upload/');
+define('DIR_CATALOG', '/Users/franklin/Sites/atix/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'simplicityuniverse.com');
+define('DB_USERNAME', 'simplja3_dba');
+define('DB_PASSWORD', '$implicitydba2015');
+define('DB_DATABASE', 'simplja3_atix');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'at_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/~franklin/atix/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/~franklin/atix/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/franklin/Sites/atix/upload/catalog/');
+define('DIR_SYSTEM', '/Users/franklin/Sites/atix/upload/system/');
+define('DIR_IMAGE', '/Users/franklin/Sites/atix/upload/image/');
+define('DIR_LANGUAGE', '/Users/franklin/Sites/atix/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Users/franklin/Sites/atix/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/franklin/Sites/atix/upload/system/config/');
+define('DIR_CACHE', '/Users/franklin/Sites/atix/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/franklin/Sites/atix/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/franklin/Sites/atix/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/franklin/Sites/atix/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/franklin/Sites/atix/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'simplicityuniverse.com');
+define('DB_USERNAME', 'simplja3_dba');
+define('DB_PASSWORD', '$implicitydba2015');
+define('DB_DATABASE', 'simplja3_atix');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'at_');
