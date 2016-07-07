4eaefaf54f31f035946427c36b15cdb53fc28499
Joshua Fabara <joshuafabara@gmail.com>
2016-07-06 20:15:09 -0500

    - all files and core for atix with new theme

---
+++ store_test/upload_bkp/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store_test.dev/admin/');
+define('HTTP_CATALOG', 'http://store_test.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store_test.dev/admin/');
+define('HTTPS_CATALOG', 'http://store_test.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/joshuafabara/Sites/store_test/upload/admin/');
+define('DIR_SYSTEM', '/Users/joshuafabara/Sites/store_test/upload/system/');
+define('DIR_IMAGE', '/Users/joshuafabara/Sites/store_test/upload/image/');
+define('DIR_LANGUAGE', '/Users/joshuafabara/Sites/store_test/upload/admin/language/');
+define('DIR_TEMPLATE', '/Users/joshuafabara/Sites/store_test/upload/admin/view/template/');
+define('DIR_CONFIG', '/Users/joshuafabara/Sites/store_test/upload/system/config/');
+define('DIR_CACHE', '/Users/joshuafabara/Sites/store_test/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/joshuafabara/Sites/store_test/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/joshuafabara/Sites/store_test/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/joshuafabara/Sites/store_test/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/joshuafabara/Sites/store_test/upload/system/storage/upload/');
+define('DIR_CATALOG', '/Users/joshuafabara/Sites/store_test/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'store_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ store_test/upload_bkp2/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store_test.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store_test.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/joshuafabara/Sites/store_test/upload/catalog/');
+define('DIR_SYSTEM', '/Users/joshuafabara/Sites/store_test/upload/system/');
+define('DIR_IMAGE', '/Users/joshuafabara/Sites/store_test/upload/image/');
+define('DIR_LANGUAGE', '/Users/joshuafabara/Sites/store_test/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Users/joshuafabara/Sites/store_test/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/joshuafabara/Sites/store_test/upload/system/config/');
+define('DIR_CACHE', '/Users/joshuafabara/Sites/store_test/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/joshuafabara/Sites/store_test/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/joshuafabara/Sites/store_test/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/joshuafabara/Sites/store_test/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/joshuafabara/Sites/store_test/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'store_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
